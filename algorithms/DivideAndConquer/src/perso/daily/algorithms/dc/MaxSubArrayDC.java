e35f01e4f81cb6723db159cb8c727a0c2170223e
jasonlee <jiancheng.li.2011@gmail.com>
2017-03-10 00:54:34 +0100

    (Algorithms) : Max Sub Array

--- algorithms/DivideAndConquer/src/perso/daily/algorithms/dc/MaxSubArrayDC.java
+++ algorithms/DivideAndConquer/src/perso/daily/algorithms/dc/MaxSubArrayDC.java
@@ -3,14 +3,52 @@ package perso.daily.algorithms.dc;
 public class MaxSubArrayDC {
 
 	public static void main(String[] args) {
-
+		int[] A = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+		
+		MaxSubArray maxSubArray = FIND_MAXIMUM_SUB_ARRAY(A, 0, A.length-1);
+		maxSubArray.print();
 	}
 
-	public static void find_maximum_subarray(int[] A, int low, int hight) {
-		
+	public static MaxSubArray FIND_MAXIMUM_SUB_ARRAY(int[] A, int low, int high) {
+		int mid = (low + high) / 2;
+		if (low == high){
+			return new MaxSubArray(low, high, A[low]);
+		}
+		else{
+			
+			MaxSubArray leftMaxSubArray = FIND_MAXIMUM_SUB_ARRAY(A, low, mid);
+			MaxSubArray rightMaxSubArray = FIND_MAXIMUM_SUB_ARRAY(A, mid+1, high);
+			MaxSubArray crossMaxSubArray = FIND_MAX_CROSSING_SUB_ARRAY(A, low, mid, high);
+			
+			if(leftMaxSubArray.getSum() > rightMaxSubArray.getSum() && leftMaxSubArray.getSum() > crossMaxSubArray.getSum())
+				return leftMaxSubArray;
+			else if(rightMaxSubArray.getSum() > leftMaxSubArray.getSum() && rightMaxSubArray.getSum() > crossMaxSubArray.getSum())
+				return rightMaxSubArray;
+			else 
+				return crossMaxSubArray;
+		}
 	}
-	
-	public static void find_max_crossing_subarray(int[] A, int low, int mid, int hight) {
+
+	public static MaxSubArray FIND_MAX_CROSSING_SUB_ARRAY(int[] A, int low, int mid, int high) {
+		int left_sum = Integer.MIN_VALUE;
+		int right_sum = Integer.MIN_VALUE;
+		int max_left = 0,  max_right = 0, sum = 0;
 		
+		for (int i = mid; i > low; i--) {
+			sum += A[i];
+			if (sum > left_sum) {
+				left_sum = sum;
+				max_left = i;
+			}
+		}
+		sum = 0;
+		for (int j = mid +1; j < high; j++) {
+			sum += A[j];
+			if (sum > right_sum) {
+				right_sum = sum;
+				max_right = j;
+			}
+		}
+		return new MaxSubArray(max_left, max_right,  left_sum + right_sum);
 	}
 }
