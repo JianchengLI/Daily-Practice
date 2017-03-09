d244e8ff85665a512cd1d2a3e817a8a8b727843d
jasonlee <jiancheng.li.2011@gmail.com>
2017-03-09 23:48:49 +0100

    (Algorithms) : Max Sub Array

---
+++ algorithms/DivideAndConquer/src/perso/daily/algorithms/dc/MaxSubArrayDC.java
@@ -0,0 +1,16 @@
+package perso.daily.algorithms.dc;
+
+public class MaxSubArrayDC {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void find_maximum_subarray(int[] A, int low, int hight) {
+		
+	}
+	
+	public static void find_max_crossing_subarray(int[] A, int low, int mid, int hight) {
+		
+	}
+}
---
+++ algorithms/DivideAndConquer/src/perso/daily/algorithms/dc/MaxSubArrayLT.java
@@ -0,0 +1,12 @@
+package perso.daily.algorithms.dc;
+/**
+ * Liner Time / Not Divide Conquer 
+ * @author Jiancheng
+ *
+ */
+public class MaxSubArrayLT {
+
+	public static void main(String[] args) {
+	}
+
+}
