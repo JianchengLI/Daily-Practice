412c255ba6eb314b78c560dba9ed16c4ce5678d8
jasonlee <jiancheng.li.2011@gmail.com>
2017-03-16 23:59:52 +0100

    (Algorithms) : SquareMatrixMultiply

--- algorithms/DivideAndConquer/src/perso/daily/algorithms/dc/SquareMatrixMultiply.java
+++ algorithms/DivideAndConquer/src/perso/daily/algorithms/dc/SquareMatrixMultiply.java
@@ -3,7 +3,21 @@ package perso.daily.algorithms.dc;
 public class SquareMatrixMultiply {
 
 	public static void main(String[] args) {
-
+		
 	}
 
+	public int[][] SquareMatrixMult(int[][] A, int[][] B){
+		int[][] C = new int[A.length][];
+		for (int i = 0; i < A.length; i++) {
+			C[i] = new int[B.length];
+			for (int j = 0; j < B.length; j++) {
+				for (int k = 0; k < B.length; k++) {
+					C[i][j] += A[i][k] * B[k][j];
+				}
+				
+			}
+		}
+		return C;
+		
+	}
 }
