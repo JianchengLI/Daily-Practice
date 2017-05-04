c9eca3826f9520e3320c249b768c6b80487a24d8
jasonlee <jiancheng.li.2011@gmail.com>
2017-05-05 00:21:20 +0200

    (Algorithms): Insert Sort

--- algorithms/LinearTimeSorting/src/perso/daily/algorithms/linearsort/BucketSort.java
+++ algorithms/LinearTimeSorting/src/perso/daily/algorithms/linearsort/BucketSort.java
@@ -1,34 +1,27 @@
 package perso.daily.algorithms.linearsort;
 
-import java.util.Comparator;
 import java.util.LinkedList;
 
 public class BucketSort {
 	public static void main(String[] argv) {
 		int A[] = new int[]{ 78, 17, 39, 26, 72, 94, 21, 12, 23, 68};
-		BUCKET_SORT(A);
+		A = BUCKET_SORT(A);
+		for (int i = 0; i < A.length; i++) {
+			System.out.print(A[i] + ", ");
+		}
 	}
 	
-	public static void BUCKET_SORT(int[] A){
+	public static int[] BUCKET_SORT(int[] A){
 		LinkedList<Integer> B[] = new LinkedList[A.length];
 		for (int i = 0; i < B.length; i++)
 			B[i] = new LinkedList<Integer>();
 		
 		for (int i = 0; i < A.length; i++) {
 			int rangeIndex = findIndexByRange(A[i], A.length);
-			insert(A[i], B[rangeIndex]);
+			insert(A[i], B[rangeIndex]); // insert sort
 		}
 		
-		for (LinkedList<Integer> linkedList : B) {
-			linkedList.sort(new Comparator<Integer>() {
-				@Override
-				public int compare(Integer o1, Integer o2) {
-					return o1.intValue() >= o2.intValue() ? 1 : 0;
-				}
-				
-			});
-		}
-		System.out.println(B);
+		return bucketsToArray(B);
 	}
 	
 	/**
@@ -41,6 +34,26 @@ public class BucketSort {
 	}
 	
 	public static void insert(int element, LinkedList<Integer> rangeList){
-		rangeList.add(element);
+		int insertIndex = 0;
+		for (int i = 0; i < rangeList.size(); i++) {
+			if(element <= rangeList.get(i).intValue()){
+				insertIndex = i;
+				break;
+			}
+		}
+		rangeList.add(insertIndex, element);
+	}
+	
+	public static int[] bucketsToArray(LinkedList<Integer> B[]) {
+		int A[] = new int[B.length]; // A.length == B.length
+		int count = 0;
+		for (int i = 0; i < B.length; i++) {
+			for (int j = 0; j < B[i].size(); j++) {
+				A[count]= B[i].get(j).intValue();
+				count ++;
+			}
+		}
+		
+		return A;
 	}
 }
