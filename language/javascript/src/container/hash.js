c291338506803e4bd2c7928182f8fd5f58fe9cb9
jasonlee <jiancheng.li.2011@gmail.com>
2017-09-19 00:18:35 +0200

    feat(DP): ES6

--- language/javascript/src/container/hash.js
+++ language/javascript/src/container/hash.js
@@ -3,4 +3,5 @@ console.log(obj.a);
 
 Object.keys(obj).forEach(key => console.log(key));
 Object.values(obj).forEach(value => console.log(value));
-Object.entries(obj).forEach( (key,value) => console.log(key + ":" +value));
+Object.entries(obj).forEach( ([key,value])=> console.log(key,value));
+
